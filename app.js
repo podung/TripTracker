266bd1e6cd849ffded32b381bf30cd0f63c7f945
Joe DePung <joe.depung@gmail.com>
2013-09-28 22:45:12 -0500

    filled out template more fully

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/'))
 
+app.configure('development', function(){
+  app.use(express.errorHandler());
+  app.locals.pretty = true;
+});
+
 app.get('/test', function (req, res) {
 	res.render('index', {title: "test"});
 });
