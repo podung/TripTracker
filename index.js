e2355ef86ff62a7d126d2881a9242a5db873aa61
Joe DePung <joe.depung@gmail.com>
2013-09-29 21:19:41 -0500

    using data from server side js in client side js

--- index.js
+++ index.js
@@ -1,83 +1,6 @@
+var TripTracker = TripTracker || {};
 
-(function () {
-	
-//	Destination
-//		Lat/Long
-//		Pics --> full size, thumbnail, description
-//		Title
-//		PathToNextDestination - empty for last location
-//			TravelType
-//			LatLong array
-	
-	//var toCologneEncoded = "i`twH{kjh@roAwX`uAwXz|@mE~lAip@`j@eP`j@_NxwAal@fcDk_AzqGmdEd|Eq|H|vE}pCbrFw_Ct|EqnB";
-	
-	var destinations = [
-		createDestination('Dusseldorf', 51.227098,6.774337, []),
-		createDestination('Cologne', 50.936796,6.960998, [new google.maps.LatLng(51.227098,6.774337), new google.maps.LatLng(50.936796,6.960998)]),
-		createDestination('Bacharach', 50.056478,7.769684, [new google.maps.LatLng(50.936796,6.960998), new google.maps.LatLng(50.056478,7.769684)]),
-		createDestination('Braubach', 50.277054,7.644371, [new google.maps.LatLng(50.057139,7.773170),
-				new google.maps.LatLng(50.066726,7.773514),
-				new google.maps.LatLng(50.074163,7.772570),
-				new google.maps.LatLng(50.076422,7.772484),
-				new google.maps.LatLng(50.079286,7.770338),
-				new google.maps.LatLng(50.084766,7.763557),
-				new google.maps.LatLng(50.087878,7.758322),
-				new google.maps.LatLng(50.102964,7.738495),
-				new google.maps.LatLng(50.108910,7.729225),
-				new google.maps.LatLng(50.114414,7.721672),
-				new google.maps.LatLng(50.124980,7.733345),
-				new google.maps.LatLng(50.128282,7.737808),
-				new google.maps.LatLng(50.132244,7.738495),
-				new google.maps.LatLng(50.135104,7.736263),
-				new google.maps.LatLng(50.136975,7.733688),
-				new google.maps.LatLng(50.137635,7.728710),
-				new google.maps.LatLng(50.139395,7.725620),
-				new google.maps.LatLng(50.142366,7.725105),
-				new google.maps.LatLng(50.146106,7.726135),
-				new google.maps.LatLng(50.147976,7.725277),
-				new google.maps.LatLng(50.150616,7.720299),
-				new google.maps.LatLng(50.153036,7.713947),
-				new google.maps.LatLng(50.156116,7.708454),
-				new google.maps.LatLng(50.163374,7.702961),
-				new google.maps.LatLng(50.166783,7.698841),
-				new google.maps.LatLng(50.170192,7.683907),
-				new google.maps.LatLng(50.170302,7.678585),
-				new google.maps.LatLng(50.170961,7.671890),
-				new google.maps.LatLng(50.173270,7.661934),
-				new google.maps.LatLng(50.174809,7.654724),
-				new google.maps.LatLng(50.177228,7.649059),
-				new google.maps.LatLng(50.180196,7.644768),
-				new google.maps.LatLng(50.184153,7.644424),
-				new google.maps.LatLng(50.188110,7.646313),
-				new google.maps.LatLng(50.193276,7.646656),
-				new google.maps.LatLng(50.200858,7.643566),
-				new google.maps.LatLng(50.204484,7.640820),
-				new google.maps.LatLng(50.209428,7.633095),
-				new google.maps.LatLng(50.214261,7.623482),
-				new google.maps.LatLng(50.220962,7.619019),
-				new google.maps.LatLng(50.227222,7.614555),
-				new google.maps.LatLng(50.231065,7.606144),
-				new google.maps.LatLng(50.234250,7.593784),
-				new google.maps.LatLng(50.234799,7.585030),
-				new google.maps.LatLng(50.236775,7.580395),
-				new google.maps.LatLng(50.240398,7.578850),
-				new google.maps.LatLng(50.243582,7.579880),
-				new google.maps.LatLng(50.245888,7.583485),
-				new google.maps.LatLng(50.247754,7.586575),
-				new google.maps.LatLng(50.248851,7.594643),
-				new google.maps.LatLng(50.249071,7.599449),
-				new google.maps.LatLng(50.247864,7.615070),
-				new google.maps.LatLng(50.245888,7.625027),
-				new google.maps.LatLng(50.247205,7.642193),
-				new google.maps.LatLng(50.251047,7.649231),
-				new google.maps.LatLng(50.255986,7.651634),
-				new google.maps.LatLng(50.261034,7.651978),
-				new google.maps.LatLng(50.266521,7.647858),
-				new google.maps.LatLng(50.270252,7.642708),
-				new google.maps.LatLng(50.272446,7.639446),
-				new google.maps.LatLng(50.273653,7.637215),
-				new google.maps.LatLng(50.274860,7.637901)])
-	];
+$(function () {
 	
 	var markers = [];
 	var map;
@@ -103,23 +26,25 @@
 	  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	  //destinations.push(createDestination('Dusseldorf', 51.227098,6.774337, google.maps.geometry.encoding.decodePath(toCologneEncoded)));
 		//dropDestinations();
-		addMarker(destinations[0]);
+		console.log(TripTracker.Destinations.length)
+		addMarker(TripTracker.Destinations[0]);
   }
 	
 	function addMarker(destination) {
 		var timeToWait = 1;
 		if (typeof destination.pathTo !== 'undefined' && destination.pathTo.length > 0)
 		{
+				var decodedPath = google.maps.geometry.encoding.decodePath(destination.pathTo);
 			
 				var pathToDisplay = new google.maps.Polyline({
-				    path: [destination.pathTo[0]],
+				    path: [decodedPath[0]],
 				    strokeColor: "#FF0000",
 				    strokeOpacity: 1.0,
 				    strokeWeight: 3
 		  	});
 		
 				var pathToFull = new google.maps.Polyline({
-				    path: destination.pathTo,
+				    path: decodedPath,
 				    strokeColor: "#FF0000",
 				    strokeOpacity: 1.0,
 				    strokeWeight: 3
@@ -128,7 +53,7 @@
 				
 				
 				
-				map.panTo(destination.pathTo[0]);
+				map.panTo(decodedPath[0]);
 				pathToDisplay.setMap(map);
 				
 				var points = pathToFull.GetPointsAtDistance(200);
@@ -173,7 +98,7 @@
 			
 		  (function(index){
 		  	setTimeout(function() {
-		      addMarker(destinations[index]);
+		      addMarker(TripTracker.Destinations[index]);
 		    }, i * 200);
 		  })(i);
 		}
@@ -208,21 +133,21 @@
 	$(function() {
 		
 			$("#nextButton").click(function(){
-				if (currentIndex < destinations.length - 1)
+				if (currentIndex < TripTracker.Destinations.length - 1)
 				{
 					currentIndex += 1;
-					addMarker(destinations[currentIndex]);
+					addMarker(TripTracker.Destinations[currentIndex]);
 				}
 			});
 			
 			$("#backButton").click(function(){
 				if (currentIndex > 0)
 				{
-					destinations[currentIndex].marker.setMap(null);
-					destinations[currentIndex].pathToDisplayPoly.setMap(null);
+					TripTracker.Destinations[currentIndex].marker.setMap(null);
+					TripTracker.Destinations[currentIndex].pathToDisplayPoly.setMap(null);
 					
 					currentIndex -= 1;
-					map.panTo(destinations[currentIndex].marker.getPosition())
+					map.panTo(TripTracker.Destinations[currentIndex].marker.getPosition())
 				}
 			})
 			
@@ -236,9 +161,4 @@
 	
 
 	
-}());
-
-
-
-
-
+});
--- scripts/destinations.js
+++ scripts/destinations.js
@@ -2,10 +2,10 @@
 	
 	function mockGetDestinationsFromDB() {
 		return [
-			mockCreateDestination('Dusseldorf', 51.227098,6.774337, "jfjsadl23"),
-			mockCreateDestination('Cologne', 50.936796,6.960998, "sdlkfjlskdjfJ@33"),
-			mockCreateDestination('Bacharach', 50.056478,7.769684, "lskdfljsdf2231"),
-			mockCreateDestination('Braubach', 50.277054,7.644371, "jfjsfdkl2010")
+			mockCreateDestination('Dusseldorf', 51.227098,6.774337, ""),
+			mockCreateDestination('Cologne', 50.936796,6.960998, "khtwHsbjh@juw@smc@"),
+			mockCreateDestination('Bacharach', 50.056478,7.769684, "_r{uHgqni@~|jDg}|C"),
+			mockCreateDestination('Braubach', 50.277054,7.644371, "cxopHiemn@}z@cAmm@zDcMP}PjLga@ji@mRv_@g}AzzBed@|x@ka@fn@aaA}gAsS}ZwWiC{P~LwJ`OcCb^_JhRqQfBkVmEuJhDoOb^cNtf@gRja@il@ha@iTvXiTh|AUh`@cCxh@mMf}@sH`l@cNjb@qQxYuWdAwWyJi_@eAkn@hRsUdP}]fo@e]b{@{h@zZcf@|ZaWps@{RflAmBtu@kK~[sUrH{RmEmMoUsJkR{Ekq@k@a]pFs`BhKg}@gGgjB_W_k@{]_No^eAia@vXiVd_@wLjSoF~LqFiC")
 		];
 	}
 	
