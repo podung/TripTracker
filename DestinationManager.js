2dddecbbf95799af017c4a2567d1329627959e17
Joe DePung <joe.depung@gmail.com>
2013-10-26 10:33:21 -0500

    broke out new MapDestination class

--- DestinationManager.js
+++ DestinationManager.js
@@ -2,99 +2,40 @@
 	
 	var DestinationManager = function(destinations, map, openSlideshow) {
 		var currentIndex = 0;
-		var waypoints = [];
 		
-		var destinations = destinations;
+		var arrayMap = Array.prototype.map;
+		
+		var destinations = arrayMap.call(destinations, function(x) {
+			return new TripTracker.MapDestination(x, map, openSlideshow);
+		});
 		
 		this.goTo = function (index) {
 			// ToDo: goTo doesn't work correctly yet...
 			currentIndex = index;
-			addMarker(destinations[index]);
+			destinations[index].start();
 		}
 		
 		this.next = function () {
 			if (currentIndex < TripTracker.Destinations.length - 1) 
 			{
+				if (currentIndex > 0)
+				{
+					destinations[currentIndex].finish();
+				}
 				currentIndex += 1;
-				addMarker(destinations[currentIndex]);
+				destinations[currentIndex].start();
 			}
 		}
 		
 		this.previous = function () {
 			if (currentIndex > 0)
 			{	
-				destinations[currentIndex].marker.setMap(null);
-				destinations[currentIndex].pathToDisplayPoly.setMap(null);
-			
+				destinations[currentIndex].hide();
+				
 				currentIndex -= 1;
-				waypoints.length = 0;
 				map.panTo(TripTracker.Destinations[currentIndex].marker.getPosition())
 			}
 		}
-		
-		function addMarker(destination) {
-		var timeToWait = 1;
-		if (typeof destination.pathTo !== 'undefined' && destination.pathTo.length > 0)
-		{
-				var decodedPath = google.maps.geometry.encoding.decodePath(destination.pathTo);
-			
-				var pathToDisplay = new google.maps.Polyline({
-				    path: [decodedPath[0]],
-				    strokeColor: "#FF0000",
-				    strokeOpacity: 1.0,
-				    strokeWeight: 3
-		  		});
-		
-				var pathToFull = new google.maps.Polyline({
-				    path: decodedPath,
-				    strokeColor: "#FF0000",
-				    strokeOpacity: 1.0,
-				    strokeWeight: 3
-		  		});
-		
-				
-				map.panTo(decodedPath[0]);
-				pathToDisplay.setMap(map);
-				
-				var points = pathToFull.GetPointsAtDistance(200);
-				waypoints.length = 0;
-				waypoints = waypoints.concat(points);
-				
-				drawNextPoint(pathToDisplay);
-		}
-
-		var marker = new google.maps.Marker({
-		      position: new google.maps.LatLng(destination.lat,destination.lng),
-		      
-		      title:destination.title
-		})
-		
-		setTimeout(function() {
-			marker.setMap(map);
-		}, 35 * waypoints.length);
-		
-		google.maps.event.addListener(marker, 'click', openSlideshow);
-		
-		destination.pathToDisplayPoly = pathToDisplay;
-		destination.marker = marker;
-	}
-	
-		function drawNextPoint(poly)
-	{
-		if (waypoints.length > 0)
-		{
-			var nextPoint = waypoints.shift();
-		
-				setTimeout(function() {
-					poly.getPath().push(nextPoint);
-		  			map.panTo(nextPoint);
-		  			
-		  			drawNextPoint(poly);
-				}, 35);
-
-		}
-	}
-	
 	}
 	
 	window.TripTracker.DestinationManager = DestinationManager;
---
+++ MapDestination.js
@@ -0,0 +1,93 @@
+(function(){
+	var MapDestination = function (destination, map, openSlideshow) {
+		var waypoints = [];
+		var destination = destination;
+		
+		this.start = function() {
+			addMarker();
+		}
+		
+		this.hide = function() {
+			// ToDo: Can this cause a race condition with drawNextPoint??
+			waypoints.length = 0;
+						
+			destination.marker.setMap(null);
+			destination.pathToDisplayPoly.setMap(null);
+		}
+		
+		this.finish = function() {
+			// ToDo: Can this cause a race condition with drawNextPoint??
+			
+			var remainingPoints = waypoints.slice();
+			waypoints.length = 0;
+			
+			var polyPath = destination.pathToDisplayPoly.getPath();
+			
+			while (remainingPoints.length > 0)
+			{
+				polyPath.push(remainingPoints.shift());
+			}
+		}
+		
+		function addMarker() {
+			var timeToWait = 1;
+			if (typeof destination.pathTo !== 'undefined' && destination.pathTo.length > 0)
+			{
+					var decodedPath = google.maps.geometry.encoding.decodePath(destination.pathTo);
+			
+					var pathToDisplay = new google.maps.Polyline({
+						path: [decodedPath[0]],
+						strokeColor: "#FF0000",
+						strokeOpacity: 1.0,
+						strokeWeight: 3
+					});
+		
+					var pathToFull = new google.maps.Polyline({
+						path: decodedPath
+					});
+		
+				
+					map.panTo(decodedPath[0]);
+					pathToDisplay.setMap(map);
+				
+					var points = pathToFull.GetPointsAtDistance(200);
+					waypoints.length = 0;
+					waypoints = waypoints.concat(points);
+				
+					drawNextPoint(pathToDisplay);
+			}
+
+			var marker = new google.maps.Marker({
+				  position: new google.maps.LatLng(destination.lat,destination.lng),
+			  
+				  title:destination.title
+			})
+		
+			setTimeout(function() {
+				marker.setMap(map);
+			}, 35 * waypoints.length);
+		
+			google.maps.event.addListener(marker, 'click', openSlideshow);
+		
+			destination.pathToDisplayPoly = pathToDisplay;
+			destination.marker = marker;
+		}
+	
+		function drawNextPoint(poly)
+		{
+			if (waypoints.length > 0)
+			{
+				var nextPoint = waypoints.shift();
+		
+					setTimeout(function() {
+						poly.getPath().push(nextPoint);
+						map.panTo(nextPoint);
+					
+						drawNextPoint(poly);
+					}, 35);
+
+			}
+		}
+	}
+	window.TripTracker.MapDestination = MapDestination;
+})()
